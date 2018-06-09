2ea8ae9d1b2e584d896ed07e873233d38a32b743
Federico Bartolomei <fbarto01@mail.bbk.ac.uk>
2018-06-09 20:17:33 +0100

    Fixed delete old backups command

--- init.el
+++ init.el
@@ -54,15 +54,10 @@
 (setq auto-save-file-name-transforms
   `((".*" ,temporary-file-directory t)))
 
-(message "Deleting old backup files...")
-(let ((week (* 60 60 24 7))
-      (current (float-time (current-time))))
-  (dolist (file (directory-files temporary-file-directory t))
-    (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
-                  week))
-      (message "%s" file)
-      (delete-file file))))
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		           		; INITIAL FRAME
